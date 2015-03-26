71691db56f1d0c32b22ee42abb35f9aee4bc10f7
Joseph Magen <jmagen@redhat.com>
2015-03-26 20:39:09 +0200

    remove bootstrap related javascript

--- fusor-ember-cli/Brocfile.js
+++ fusor-ember-cli/Brocfile.js
@@ -4,7 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp({
   'ember-cli-bootstrap-sass': {
-    'importBootstrapJS': true
+    'importBootstrapJS': false
   },
   storeConfigInMeta: false
 });
